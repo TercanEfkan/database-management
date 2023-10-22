b7bf5c5610f20d94272e24a9f02c42c280477d2e
Efkan Kıraç Tercan <92805396+TercanEfkan@users.noreply.github.com>
2023-10-22 12:35:10 +0300

    less volume

--- blocks.js
+++ blocks.js
@@ -18,7 +18,9 @@ let currentIndex = 0;
 const start = document.getElementById("startSound");
 const collisionSound = document.getElementById("collisionSound");
 const miss = document.getElementById("missSound");
-
+start.volume = 0.1;
+collisionSound.volume = 0.1;
+miss.volume = 0.1;
 class Block {
     constructor(toughness, row, col, type) {
         this.row = row + 1;
