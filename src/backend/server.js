69c4c443cb30d7d648b12e382a1ffdb5f2a89974
Efkan Kıraç Tercan <92805396+TercanEfkan@users.noreply.github.com>
2023-12-10 16:14:56 +0300

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -20,6 +20,8 @@ app.post('/signup', async (req, res) => {
         const pool = await poolPromise;
         const request = pool.request();
         // Example query to insert data into a table
+        const countResult = await request.query(`SELECT COUNT(*) AS rowCount FROM dbo.Player WHERE UserID LIKE '${username}'`);
+        console.log('Row count:', countResult.recordset[0].rowCount); // Log the row count
         await request.query(`INSERT INTO dbo.Player (UserID, Password, PlayerName) VALUES ('${username}', '${password}', '${playername}')`);
 
         res.status(201).json({ message: 'Sign-up successful' });
