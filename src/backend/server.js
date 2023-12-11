fb7c598d4ad798bc7d40a362d61e6a6ee4dd8353
Efkan Kıraç Tercan <92805396+TercanEfkan@users.noreply.github.com>
2023-12-11 16:14:34 +0300

    database username check

--- src/backend/server.js
+++ src/backend/server.js
@@ -20,11 +20,19 @@ app.post('/signup', async (req, res) => {
         const pool = await poolPromise;
         const request = pool.request();
         // Example query to insert data into a table
-        const countResult = await request.query(`SELECT COUNT(*) AS rowCount FROM dbo.Player WHERE UserID LIKE '${username}'`);
-        console.log('Row count:', countResult.recordset[0].rowCount); // Log the row count
-        await request.query(`INSERT INTO dbo.Player (UserID, Password, PlayerName) VALUES ('${username}', '${password}', '${playername}')`);
+        const countResult = await request.query(`SELECT COUNT(*) FROM dbo.Player WHERE UserID = '${username}'`);
+        console.log("amount of rows matched: " + countResult.recordset[0]['']);
 
-        res.status(201).json({ message: 'Sign-up successful' });
+        if (countResult.recordset[0][''] === 0){
+            await request.query(`INSERT INTO dbo.Player (UserID, Password, PlayerName) VALUES ('${username}', '${password}', '${playername}')`);
+
+            res.status(201).json({ message: 'Sign-up successful' });
+        }
+        else{
+            console.error('Error: username unavailable')
+            res.status(201).json({ message: 'Username is unavailable' });
+        }
+
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Internal server error' });
